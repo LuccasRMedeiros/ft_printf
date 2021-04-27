a3201e8bf41e886a73b0a1a872097126e0d03b43
Luccas Rocigno <luccasrocigno@hotmail.com>
2021-04-27 13:50:59 -0300

    submodule

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: lrocignoS <lrocigno@student.42sp.org.br    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/01 19:01:05 by lrocigno          #+#    #+#             */
-/*   Updated: 2021/04/24 17:59:12 by lrocignoS        ###   ########.fr       */
+/*   Updated: 2021/04/25 14:05:45 by lrocignoS        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,14 +21,14 @@ int	main(void)
 	int		ret;
 	int		pf_ret;
 	char	*phrase;
-	char	*stest;
+	//char	*stest;
 	int		ntest;
 
-	phrase = "%-015.7d %-.19s %%\e[0;32mend\e[0m\n";
-	stest = "ok!";
+	phrase = "%c:\t|%-*d|\n";
+	//stest = "ok!";
 	ntest = 9831;
-	ret = ft_printf(phrase, ntest, stest);
-	pf_ret = printf(phrase, ntest, stest);
+	ret = ft_printf(phrase, 'f', 19, ntest);
+	pf_ret = printf(phrase, 'o', 19, ntest);
 	printf("Return of ft_printf: \e[1;33m%d\e[0m\n", ret);
 	printf("Return of printf: \e[1;33m%d\e[0m\n", pf_ret);
 	return (0);
