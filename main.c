9358c4e9fa064c2940d17c3caffb8590192c77ea
Luccas Rocigno <luccasrocigno@hotmail.com>
2021-05-03 12:53:59 -0300

    test

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: lrocigno <lrocigno@student.42sp.org>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/01 19:01:05 by lrocigno          #+#    #+#             */
-/*   Updated: 2021/05/03 11:26:40 by lrocigno         ###   ########.fr       */
+/*   Updated: 2021/05/03 12:53:41 by lrocigno         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,13 +22,10 @@ int	main(void)
 {
 	int		pf_ret;
 	int		ret;
-	//char	*phrase;
-
-	//phrase = ft_strdup("ultimate2 %*d %*s %*x %*X %*i %*u\n");
-	ret = ft_printf(" *%-*.*x* *%*.*x* \n", 4, 5, 10, 10, 21, -10);
-	pf_ret = printf(" *%-*.*x* *%*.*x* \n", 4, 5, 10, 10, 21, -10);
+	
+	ret = ft_printf(" %p %p ", LONG_MIN, LONG_MAX);
+	pf_ret = printf(" %p %p ", LONG_MIN, LONG_MAX);
 	printf("Return of ft_printf: \e[1;33m%d\e[0m\n", ret);
 	printf("Return of printf: \e[1;33m%d\e[0m\n", pf_ret);
-	//free(phrase);
 	return (0);
 }
