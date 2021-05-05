2562875dcafd3fbf842d5eb460cdea0326c2456d
Luccas Rocigno de medeiros <lrocigno@c1r1p4.42sp.org.br>
2021-05-05 12:42:20 -0300

    new bug

--- src/pf_error.c
+++ src/pf_error.c
@@ -6,7 +6,7 @@
 /*   By: lrocigno <lrocigno@student.42sp.org>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/05 10:21:01 by lrocigno          #+#    #+#             */
-/*   Updated: 2021/05/05 12:10:09 by lrocigno         ###   ########.fr       */
+/*   Updated: 2021/05/05 12:41:11 by lrocigno         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ bool	pf_error(const char *str)
 	{
 		++i;
 		if (ft_strhvchr(P_SPECS, str[i]))
-			pf_error(test + i);
+			pf_error(test + i + 1);
 	}
 	return (true);
 }
